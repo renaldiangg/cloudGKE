6719dd31ef319827809a2c02f04867e36f38437a
renaldiangg <103799681+renaldiangg@users.noreply.github.com>
2022-04-17 21:53:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world! Sampurna Panjiwinata 52019034 Jurusan Teknik Informatika\n')
+    res.send('Hello world! Renaldi Anggarkusuma 52019028 Jurusan Teknik Informatika\n')
 })
 
 app.listen(PORT, HOST)
